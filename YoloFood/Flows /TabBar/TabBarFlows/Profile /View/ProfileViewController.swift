9d89ab13a5a041a9f560d0043b56db631e8619b2
Agatay <beshssg@gmail.com>
2022-03-13 04:26:48 +0600

    FEA: Добавлена стартовая страница регистрации

--- YoloFood/Flows /TabBar/TabBarFlows/Profile /View/ProfileViewController.swift
+++ YoloFood/Flows /TabBar/TabBarFlows/Profile /View/ProfileViewController.swift
@@ -5,11 +5,63 @@
 //  Created by Tanirbergen Kaldybay on 21.02.2022.
 //
 
-import Foundation
+import UIKit
 
 final class ProfileViewController: BaseViewController {
     
+// MARK: - UI Elements:
+    
+    fileprivate lazy var tableView: UITableView = {
+        return $0
+    }(UITableView())
+    
+// MARK: - Lifecycle:
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
+    }
+}
+
+// MARK: - ConfigureUI:
+
+extension ProfileViewController {
+    
+    func configureUI() {
+        [view].forEach {
+            $0?.backgroundColor = .white
+            $0?.addSubview(tableView)
+        }
+        
+        [tableView].forEach {
+            $0.register(ProfileTableViewCell.self, forCellReuseIdentifier: String(describing: ProfileTableViewCell.self))
+            $0.delegate = self
+            $0.dataSource = self
+            
+            $0.separatorStyle = .none
+        }
+        
+        makeConstraints()
+    }
+    
+    func makeConstraints() {
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
+
+// MARK: - TableViewDelegate / DataSource:
+
+extension ProfileViewController: TableViewDelegateProtocol {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: ProfileTableViewCell.self), for: indexPath) as! ProfileTableViewCell
+        cell.selectionStyle = .none
+        return cell
     }
 }
